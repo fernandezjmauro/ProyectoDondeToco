e4bd327fe4116f5e97bd1e9c6b2bde71a684d0ca
fernandezjmauro <145069783+fernandezjmauro@users.noreply.github.com>
2023-11-22 21:05:21 -0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
+let formulario = document.getElementsByName("formulario")
+
+let validarNombre = function (e) {
+  if ((formulario.nombre.value == "")) {
+      alert("Faltan completar espacios del formulario")
+      e.preventDefault()
+      }
+  }
+
+let validar = function (e) {  // "e" es el evento recibido del form (https://developer.mozilla.org/es/docs/Web/API/Event/preventDefault)
+  validarNombre(e)
+  }
+
+
 function iniciarMap(){
     var coord = {lat:-34.6027394 ,lng: -58.420759};
     var map = new google.maps.Map(document.getElementById('map'),{
@@ -9,6 +23,8 @@ function iniciarMap(){
       map: map
     });
 }
+
+formulario.addEventListener("submit", validar)
 
 function iniciarMap2(){
   var coord = {lat:-34.596046 ,lng: -58.393559};
